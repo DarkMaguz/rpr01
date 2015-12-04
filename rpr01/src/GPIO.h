28faf91fb4355f9aeff52b14a5ead53901cd1ee3
The Dark Mage <magus@darkmagus.dk>
2015-12-04 18:21:06 +0100

    Første commit af rpr01.

---
+++ rpr01/src/GPIO.h
@@ -0,0 +1,37 @@
+/*
+ * GPIO.h
+ *
+ *  Created on: 24/11/2015
+ *      Author: magnus
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include <string>
+using namespace std;
+
+/* GPIO Class
+ * Purpose: Each object instantiated from this class will control a GPIO pin
+ * The GPIO pin number must be passed to the overloaded class constructor
+ */
+class GPIO
+{
+	public:
+		GPIO(); // create a GPIO object that controls GPIO4 (default
+		GPIO( string x ); // create a GPIO object that controls GPIOx, where x is passed to this constructor
+		virtual ~GPIO();
+		
+		int export_gpio(); // exports GPIO
+		int unexport_gpio(); // unexport GPIO
+		int setdir_gpio( string dir ); // Set GPIO Direction
+		int setval_gpio( string val ); // Set GPIO Value (putput pins)
+		int getval_gpio( string& val ); // Get GPIO Value (input/ output pins)
+		string get_gpionum(); // return the GPIO number associated with the instance of an object
+		
+	private:
+		string gpionum; // GPIO number associated with the instance of an object
+		
+};
+
+#endif /* GPIO_H_ */
---
+++ rpr01/src/rpr01.cpp
@@ -0,0 +1,47 @@
+/*
+ ============================================================================
+ Name        : rpr01.cpp
+ Author      : me
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+#include <unistd.h>
+#include "GPIO.h"
+
+using namespace std;
+
+int main( int argc, char **argv )
+{
+	
+	GPIO* gpio4 = new GPIO( "4" ); // Lav et nyt GPIO object med tilknytning til GPIO4(PIN 7) den røde LED
+	
+	cout << "Eksportere GPIO pins... ";
+	cout.flush();
+	gpio4->export_gpio(); // Eksporter GPIO4(PIN 7)
+	cout << "Færdig!" << endl;
+	
+	cout << "Sætter GPIO pin retningen... ";
+	cout.flush();
+	gpio4->setdir_gpio( "out" ); // GPIO4(PIN 7) sat som output pin
+	cout << "Færdig!" << endl;
+	
+	gpio4->setval_gpio( "1" ); // Tænd for de røde LED
+	sleep( 10 );
+	gpio4->setval_gpio( "0" ); // Sluk for de røde LED
+	
+	cout << "Afeksportere GPIO pins... ";
+	cout.flush();
+	gpio4->unexport_gpio(); //Afeksporter GPIO4(PIN 7)
+	cout << "Færdig!" << endl;
+	
+	delete gpio4;
+	
+	cout << "Afslutter programmet..." << endl;
+	
+	return 0;
+	
+}
