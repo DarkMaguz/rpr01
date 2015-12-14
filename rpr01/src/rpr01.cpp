394b32e74c62f29304c434df9ea19a4ec9baa188
The Dark Mage <magus@darkmagus.dk>
2015-12-14 22:00:05 +0100

    tester

--- rpr01/src/rpr01.cpp
+++ rpr01/src/rpr01.cpp
@@ -28,6 +28,7 @@ void KeyPressed( int c, void *data )
 			break;
 		case 's': // Kør tilbage.
 			gpio->SetHigh( 18 );
+			gpio->SetHigh( 17 );
 			//gpio->SetHigh( 23 );
 			cout << "kør tilbage.";
 			break;
